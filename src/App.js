e571b875bd8360e9bcd3d22f369fd7b84f1b143d
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-06-16 00:02:09 +0530

    finishing dropdown component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -16,7 +17,30 @@ const items = [
         content: 'You use React by creating components'
     },
 ]
+
+const options = [
+    {
+        label:'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+    ,
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+]
 export default () =>
 {
-    return <div><Search/></div>
+    const [selected,setSelected]=useState(options[0]);
+    return (
+        <div>
+            <Dropdown selected={selected}
+            onSelectedChange={setSelected}
+            options = {options}/>
+        </div>
+    );
 };
