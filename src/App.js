0154d07812f9b37924f34626939da7336ad30913
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-06-16 21:52:38 +0530

    Translation Widget Finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React,{useState} from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework.'
-    },
-    {
-        title: 'Why use React?',
-        content : 'React is a favorite JS library among developers'
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use React by creating components'
-    },
-]
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among developers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
-    {
-        label:'The Color Red',
-        value: 'red'
-    },
-    {
-        label: 'The Color Blue',
-        value: 'blue'
-    }
-    ,
-    {
-        label: 'The Color Green',
-        value: 'green'
-    },
-]
-export default () =>
-{
-    const [selected,setSelected]=useState(options[0]);
-    return (
-        <div>
-            <Dropdown selected={selected}
-            onSelectedChange={setSelected}
-            options = {options}/>
-        </div>
-    );
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+];
+export default () => {
+  return (
+    <div>
+      <Translate />
+    </div>
+  );
 };
